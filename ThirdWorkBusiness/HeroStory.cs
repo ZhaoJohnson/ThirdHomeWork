9d387df840f46ce6347d9c586ba58e8367d5a34c
Zhao.Johnson <johnson.zhao@starlight-sms.com>
2016-10-21 20:25:27 +0800

    first

---
+++ ThirdWorkBusiness/HeroStory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThirdWorkInterFace.IBusiness;
+using ThirdWorkModel;
+
+namespace ThirdWorkBusiness
+{
+    public class HeroStory : IHeroStory
+    {
+        public Hero MyHero { get; set; }
+        public List<Task> MyHeroStory { get; set; }
+        public void Dispose()
+        {
+        }
+    }
+}
